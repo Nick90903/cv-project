fa7780f7f6e9767a516754dee784b1a006b58d7f
nick90903 <nhatchew03@gmail.com>
2022-07-29 00:56:31 -0500

    Adds inputs pages and sets up boilerplate for reading states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import LoadOutputs from "./components/Outputs";
 
 function App() {
   return (
     <div className="App">
-      <p> Hi </p>
+      <LoadOutputs title="Hello World!" />
     </div>
   );
 }
--- src/components/Outputs.js
+++ src/components/Outputs.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+function LoadOutputs(props) {
+  const [count, setCount] = useState("");
 
-function LoadOutputs() {
+  // Add states for each of the items. Set these states in their own Component
+  // and load/render the outputs page here. Pass the states value from their
+  // own component over to this script. This is a script to display the values
+  // AFTER they have been set and a value assigned in THEIR OWN COMPONENTS
+  //
+
   return (
     <div className="Outputs">
-      <div></div>
+      <div>{props.title}</div>
+      <button onClick={() => setCount("clicked")}>Click me</button>
+      <p>You clicked {count} times</p>
     </div>
   );
 }
